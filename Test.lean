cbb30db6fe985f75a159103507bb64f2a8e722f7
drcicero <11000518+drcicero@users.noreply.github.com>
2024-08-03 18:06:49 +0200

    fix

--- Test.lean
+++ Test.lean
@@ -13,6 +13,13 @@ def Tpe.toQuery : Tpe → String
   | TXT => "TEXT"
   | BLB => "BLOB"
 
+abbrev Tpe.toType : Tpe → Type
+  | NUL => Unit
+  | INT => UInt32
+  | FLT => Float
+  | TXT => String
+  | BLB => String
+
 inductive Val : Tpe → Type where
   | n : Val .NUL
   | i : UInt32 → Val .INT
@@ -28,6 +35,9 @@ def Any.toString : Any → String := fun a => a.2.toString
 instance: ToString Any where toString := Any.toString
 
 instance : CoeOut (Val α) Any where coe a := ⟨α, a⟩
+
+-------------------------------------------------------------------------------
+-- Sqlite
 
 def Sqlite.Cursor.getTpe (c: Cursor) (iCol: UInt32): IO Tpe :=
   return match (← c.getType iCol).val.1 with
@@ -47,7 +57,7 @@ def Sqlite.Cursor.readVal (c: Cursor) (iCol: UInt32): {α: Tpe} → IO (Val α)
 def Sqlite.Cursor.readAny (c: Cursor) (iCol: UInt32): IO Any :=
   do let t := ← c.getTpe iCol; return ⟨ t, ← c.readVal iCol (α:=t) ⟩
 
-def Sqlite.Cursor.exec : Cursor -> IO PUnit := fun cur => do
+def Sqlite.Cursor.exec : Cursor -> IO Unit := fun cur => do
   let count <- cur.reads
   let mut more <- cur.step
   let header: List (String × Tpe) <- (List.range count.toNat).mapM fun i =>
@@ -63,14 +73,38 @@ def Sqlite.Cursor.exec : Cursor -> IO PUnit := fun cur => do
     more <- cur.step
   return ()
 
-def Sqlite.Db.exec : Db -> String -> IO PUnit := fun db stmt => do
+def Sqlite.Db.exec : Db -> String -> IO Unit := fun db stmt => do
   IO.println s!"> {stmt}"
   let cur <- db.prep stmt
   cur.exec
 
--- def Sqlite.Db.createIfNotExists : Tab
--- def Sqlite.Db.insert : Db → IO Unit := _
--- def Sqlite.Db.select : Db → IO Unit := _
+-------------------------------------------------------------------------------
+-- Signature and Table
+
+def Sig : Type := List (String × Tpe)
+abbrev Sig.toType : Sig → Type
+  | []    => Unit
+  | x::xs => Prod x.2.toType (Sig.toType xs)
+def Sig.toQuery (xs: Sig): String :=
+  ", ".intercalate (xs.map fun x => x.1 ++ " " ++ x.2.toQuery)
+
+structure Tab (s: Sig): Type where
+  db   : Sqlite.Db
+  name : String
+
+abbrev ReadCursor (s: Sig) := Sqlite.Cursor
+abbrev WriteCursor (s: Sig) := Sqlite.Cursor
+
+def Sqlite.Db.getOrCreateTable (db: Db) (name: String) (sig: Sig): IO (Tab sig) := do
+  db.exec s!"CREATE TABLE IF NOT EXISTS {name}({sig.toQuery})"
+  return { db := db, name := name }
+def Tab.prepInsert (tab: Tab s): IO (WriteCursor s) := do
+  let list := ", ".intercalate (List.range s.length |>.map fun _ => "?")
+  tab.db.prep s!"INSERT INTO {tab.name} VALUES ({list})"
+def Tab.prepSelect (tab: Tab s): IO (ReadCursor s) := do
+  let s := s!"SELECT * FROM {tab.name}"
+  IO.println s!"> {s}"
+  tab.db.prep s
 
 def Sqlite.Cursor.bind (c: Cursor) (xs: List Any): IO Unit := do
   --let max ← c.binds
@@ -86,26 +120,21 @@ def Sqlite.Cursor.bind (c: Cursor) (xs: List Any): IO Unit := do
   let (false) <- c.step | throw (IO.userError "bind not done")
   c.boot -- reset
 
-structure Sig : Type where
-  columns: List (String × Tpe)
-def Sig.toQuery (xs: Sig): String :=
-  ", ".intercalate (xs.columns.map fun x => x.1 ++ " " ++ x.2.toQuery)
+def WriteCursor.write (c: WriteCursor s) (iCol: UInt32) (vs: s.toType): IO Unit := match s, vs with
+  | ⟨_,a⟩::ss, (v, vs) => do
+    match a with
+      | .NUL => c.bindNull   iCol
+      | .INT => c.bindUInt32 iCol v
+      | .FLT => c.bindFloat  iCol v
+      | .TXT => c.bindString iCol v
+      | .BLB => c.bindString iCol v
+    c.write (s:=ss) (iCol+1) vs
+  | [], () => do
+    let (false) <- c.step | throw (IO.userError "bind not done")
+    c.boot -- reset
 
-structure Tab (s: Sig): Type where
-  db   : Sqlite.Db
-  name : String
-  sig  : Sig
-
-def Sqlite.Db.getOrCreateTable (db: Db) (name: String) (sig: Sig): IO (Tab sig) := do
-  db.exec s!"CREATE TABLE IF NOT EXISTS {name}({sig.toQuery})"
-  return { db := db, name := name, sig := sig }
-def Tab.prepInsert (tab: Tab s): IO Sqlite.Cursor := do
-  let list := ", ".intercalate (List.range tab.sig.columns.length |>.map fun i => "?")
-  tab.db.prep s!"INSERT INTO {tab.name} VALUES ({list})"
-def Tab.prepSelect (tab: Tab s): IO Sqlite.Cursor := do
-  let s := s!"SELECT * FROM {tab.name}"
-  IO.println s!"> {s}"
-  tab.db.prep s
+-------------------------------------------------------------------------------
+-- main
 
 def main : IO Unit := do
   IO.println <| <- Sqlite.version
@@ -114,17 +143,17 @@ def main : IO Unit := do
   db.exec "SELECT SQLITE_VERSION()"
   --db.exec "DROP TABLE IF EXISTS cars"
 
-  let Car  := Sig.mk [("id", .INT), ("name", .TXT), ("price", .INT)]
+  let Car: Sig := [("id", .INT), ("name", .TXT), ("price", .INT)]
   let cars <- db.getOrCreateTable "cars" Car
   let cur  <- cars.prepInsert
-  cur.bind [Val.i 1, Val.t "Audi",       Val.i  52642]
-  cur.bind [Val.i 2, Val.t "Mercedes",   Val.i  57127]
-  cur.bind [Val.i 3, Val.t "Skoda",      Val.i   9000]
-  cur.bind [Val.i 4, Val.t "Volvo",      Val.i  29000]
-  cur.bind [Val.i 5, Val.t "Bentley",    Val.i 350000]
-  cur.bind [Val.i 6, Val.t "Citroen",    Val.i  21000]
-  cur.bind [Val.i 7, Val.t "Hummer",     Val.i  41400]
-  cur.bind [Val.i 8, Val.t "Volkswagen", Val.i  21600]
+  cur.write 1 (1, "Audi",        52642, ())
+  cur.write 1 (2, "Mercedes",    57127, ())
+  cur.write 1 (3, "Skoda",        9000, ())
+  cur.write 1 (4, "Volvo",       29000, ())
+  cur.write 1 (5, "Bentley",    350000, ())
+  cur.write 1 (6, "Citroen",     21000, ())
+  cur.write 1 (7, "Hummer",      41400, ())
+  cur.write 1 (8, "Volkswagen",  21600, ())
 
   db.exec "SELECT * FROM sqlite_master"
 
